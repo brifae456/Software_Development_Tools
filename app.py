d1f85d4efc65303d9c0a0b2e65ddd115354bffb3
brifae456 <bkg41383@gmail.com>
2024-10-17 22:57:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,48 +15,81 @@ df = pd.read_csv('./vehicles_us_cleaned.csv')
 # Show data in the app
 st.write(df)
 
-# histogram of the types of vehicles by manufacturer
-st.subheader('Histogram of the types of vehicles by manufacturer')
-fig = px.histogram(df, x='manufacturer', color='type')
-# plot the histogram
-st.plotly_chart(fig)
-
-# histogram of price distribution between manufacturers
-st.subheader('Histogram of price distribution between manufacturers')
-# drop down menu for selecting the manufacturer 1 and 2 
-# index 1 and 2 are used to set default values for the drop down menu
-manufacturer1 = st.selectbox('Manufacturer 1', df['manufacturer'].unique(), index=1)
-manufacturer2 = st.selectbox('Manufacturer 2', df['manufacturer'].unique(), index=2)
-# create a normalized histogram checkbox
-normalized = st.checkbox('Normalized')
-# create a histogram with manufacturer1 and manufacturer2 input
-fig = px.histogram()
-fig.add_trace(go.Histogram(x=df[df['manufacturer'] == manufacturer1]['price'], name=manufacturer1, opacity=0.75, histnorm='percent'))
-fig.add_trace(go.Histogram(x=df[df['manufacturer'] == manufacturer2]['price'], name=manufacturer2, opacity=0.75, histnorm='percent'))
-# normalize the histogram if the checkbox is checked
-if normalized:
-    fig.update_layout(barmode='overlay')
-    fig.update_traces(opacity=0.75)
-# x-axis title
-fig.update_xaxes(title_text='Price')
-# y-axis title
-fig.update_yaxes(title_text='Percentage')
-# plot the histogram
-st.plotly_chart(fig)
+# cleaning up 'model_year' column
+# filling in the missing values in 'model_year' with a median for a group based on 'model'
+df['model_year'] = df['model_year'].fillna(df.groupby(['model'])['model_year'].transform('median'))
+df['model_year'] = df['model_year'].astype(int)
+df['model_year'] = df['model_year'].astype(str)
 
 
+# cleaning up 'cylinders' column
+# filling in the missing values in 'cylinders' with a median for a group based on 'model'
+df['cylinders'] = df['cylinders'].fillna(df.groupby(['model'])['cylinders'].transform('median'))
+df['cylinders'] = df['cylinders'].astype(int)
 
-# scatter plot matrix 
-st.subheader('Scatter plot matrix')
-# drop down for each dimension 
-# index 1, 2, and 3 are used to set default values for the drop down menu
-x_axis = st.selectbox('X axis', df.columns, index=1)
-y_axis = st.selectbox('Y axis', df.columns, index=2)
-# drop down for the color
-color = st.selectbox('Color', df.columns, index=3)
-# subheader for the scatter plot matrix that automatically updates
-st.subheader(f'Scatter plot matrix of {x_axis} and {y_axis} by {color}')
-# create the scatter plot matrix
-fig = px.scatter_matrix(df, dimensions=[x_axis, y_axis], color=color)
-# plot the scatter plot matrix
-st.plotly_chart(fig)
+
+# cleaning up 'odometer' column
+# filling in the missing values in 'odometer' with a median for a group based on 'model'
+df['odometer'] = df['odometer'].fillna(df.groupby(['model'])['odometer'].transform('median'))
+
+
+# cleaning up 'paint_color' column
+df['paint_color'].fillna('unknown', inplace=True)
+
+
+# cleaning up 'is_4wd' column
+df['is_4wd'].fillna('no', inplace=True)
+df['is_4wd'].replace(1.0, 'yes', inplace=True)
+
+
+st.header("Data Viewer")
+st.write("""
+###### Check out car listings here!
+""")
+
+# Display car listings
+st.dataframe(df, height=300)
+
+
+st.header("Condition vs Model Year")
+st.write("""
+###### Check the distribution of car conditions across model years.
+""")
+
+# Filter out model years equal to 0
+filtered_df = df[df['model_year'] != '0']
+
+# Group by manufacturer and vehicle type, and count the number of vehicles
+grouped_df = filtered_df.groupby(['model_year', 'condition']).size().reset_index(name='count')
+
+# Create bar chart
+fig1 = px.histogram(grouped_df, x="model_year", y="count", color="condition")
+
+st.plotly_chart(fig1)
+
+
+# defining age category of car
+df['age']= 2024 - (pd.to_numeric(filtered_df['model_year'], errors='coerce'))
+def age_category(x):
+    if x < 10: return '<10 yrs old'
+    elif 10 <= x < 20: return '10-20 yrs old'
+    else: return '20+ yrs old'
+
+df['age_category'] = df['age'].apply(age_category)
+
+
+st.header('Price vs Mileage')
+st.write("""
+###### Check how price is affected by a car's mileage.
+""")
+
+# Checkbox to enable/disable visual distinction between ages
+show_age_category = st.checkbox("Show Age Categories")
+
+# Distribution of price depending on odometer_value, engine_capacity, number_of_photos
+if show_age_category:
+    fig2 = px.scatter(df, x="price", y="odometer", color="age_category")
+else:
+    fig2 = px.scatter(df, x="price", y="odometer")
+
+st.plotly_chart(fig2)
